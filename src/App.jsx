77a644aae5458b137443e21ef5f41d6c7729f59a
Bryce Billing <brycerbilling@gmail.com>
2025-02-18 11:02:52 +0200

    Added privacy policy etc

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import {ChakraProvider, extendTheme} from "@chakra-ui/react";
 import Home from "./pages/Home"; // Your main landing page
 import Terms from "./pages/Terms"; // The new Terms component
 import Privacy from "./pages/Privacy"; // The new Privacy component
-import Eula from "./pages/Eula.jsx"; // The new EULA component
+import Eula from "./pages/Eula.jsx";
+import Disclaimer from "./pages/Disclaimer.jsx"; // The new EULA component
 
 const theme = extendTheme({
     fonts: {
@@ -28,6 +29,7 @@ function App() {
                     <Route path="/butterflies-of-zimbabwe/terms-and-conditions" element={<Terms/>}/>
                     <Route path="/butterflies-of-zimbabwe/privacy-policy" element={<Privacy/>}/>
                     <Route path="/butterflies-of-zimbabwe/eula" element={<Eula/>}/>
+                    <Route path="/disclaimer" element={<Disclaimer/>}/>
                 </Routes>
             </Router>
         </ChakraProvider>
